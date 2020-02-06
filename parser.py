d225e47375c7b081611961d9178d7a6e046fa50a
Szabó Bence <szabo.bence.tat@gmail.com>
2020-02-06 14:16:39 +0100

    csv and xlsx readings

--- parser.py
+++ parser.py
@@ -39,3 +39,26 @@ def parse_out_of_stock_days(df):
 
 def parse_sku_mapping(df):
     return df
+
+
+def read_xlsx(filename):
+    df = pd.read_excel(filename)
+    df.drop(df.columns[0], axis=1, inplace=True)
+
+    # df['Year'] = pd.DatetimeIndex(df['Date']).year.astype(int)
+    # df['Month'] = pd.DatetimeIndex(df['Date']).strftime('%B')
+    # df['Day'] = pd.DatetimeIndex(df['Date']).day.astype(int)
+    # df = df[['Year', 'Month', 'Day', 'Market Place', 'ASIN', 'Units', 'Refunded', 'PPC Orders']]
+    # df.to_excel('sales.xlsx', sheet_name='sales')
+
+    return df
+
+
+def read_csv(filename):
+    df = pd.read_csv(filename)
+    df = df[['Market Place', 'ASIN', 'Out of stock days']]
+    df['Year'] = 2019
+    df['Month'] = 'January'
+    # FIXME
+
+    return df
