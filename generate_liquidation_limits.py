84911df17840408b5ba72a331f53f90d0dcb7cbc
Szabó Bence <szabo.bence.tat@gmail.com>
2020-02-10 07:24:17 +0100

    packed generate_liquidation_limits separately

--- generate_liquidation_limits.py
+++ generate_liquidation_limits.py
@@ -1,6 +1,10 @@
 import pandas as pd
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+from dotenv import load_dotenv
+
+from parser import *
+from gservice import *
 
 
 def generate_liquidation_limits(cin7_df, start_date_string, end_date_string):
@@ -18,3 +22,19 @@ def generate_liquidation_limits(cin7_df, start_date_string, end_date_string):
         iterator_date += relativedelta(months=1)
 
     return liquidation_limits[['Cin7', 'Year', 'Month', 'Normal Price', 'Liquidation Limit']]
+
+
+def main():
+    load_dotenv()
+    cin7_product = get_data_from_spreadsheet(os.getenv('INPUT_SPREADSHEET_ID'), 'Input-Cin7-Product-Map')
+    liquidation_limits = generate_liquidation_limits(cin7_product, '2017.01.01.', '2020.01.31.')
+
+    upload_data_to_sheet(
+        format_for_google_sheet_upload(liquidation_limits),
+        os.getenv('INPUT_SPREADSHEET_ID'),
+        'Input-Liquidation-Limits'
+    )
+
+
+if __name__ == '__main__':
+    main()
